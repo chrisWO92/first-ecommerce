b4e11878376079df60d6d09288a392af7a6a731f
chris <chrisworkingonline@gmail.com>
2023-10-24 22:46:25 -0300

    layout

---
+++ src/pages/App/index.jsx
@@ -0,0 +1,33 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+import Home from '../Home'
+import MyAccount from '../MyAccount'
+import MyOrder from '../MyOrder'
+import MyOrders from '../MyOrders'
+import NotFound from '../NotFound'
+import SignIn from '../SignIn'
+import Navbar from '../../components/Navbar'
+import './App.css'
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/*', element: <NotFound /> },
+    { path: '/sing-in', element: <SignIn /> },
+  ])
+  return routes
+}
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <AppRoutes />
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const SignIn = () => {
+  return (
+    <Layout>
+      Sign In
+    </Layout>
+  )
+}
+
+export default SignIn
